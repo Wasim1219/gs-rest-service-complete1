c5dc9af94d3573b1e2a7d8b1436555879b64a954
Wasim <wdeshmukh7@gmail.com>
2020-01-02 17:27:08 +0530

    some updates about put and get

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,6 +1,9 @@
 package hello;
 
 import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +14,20 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+	/*
+	 * @RequestMapping("/greeting") public Greeting
+	 * greeting(@RequestParam(value="name", defaultValue="World") String name) {
+	 * return new Greeting(counter.incrementAndGet(), String.format(template,
+	 * name)); }
+	 */
+	
+	@GetMapping("/greeting")
+	public Greeting greeting1(@RequestParam(value="name", defaultValue="World") String name) {
+		return new Greeting(counter.incrementAndGet(),
+							String.format(template, name));
+	}
+	@PutMapping("/greetings")
+	public Greeting greeting2(@RequestParam(value="name", defaultValue="World") String name) {
 		return new Greeting(counter.incrementAndGet(),
 							String.format(template, name));
 	}
